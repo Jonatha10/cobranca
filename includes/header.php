1f34afa3c115d61b017c50045453055535c68681
Jonatha10 <jonatha949@gmail.com>
2022-07-22 01:22:44 -0300

    ALTERAÇÃO HEADER

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <!-- BOOTSTRAP 4 -->
   <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
-  <!-- FONT AWESOEM -->
+  <!-- FONT AWESOME -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 
@@ -15,8 +15,7 @@
 
 <div class="p-3 mb-2 bg-dark text-white">
     <div class="container ">
-    <h2 class="text-center">Sistema de Cobrança</h2>
-
+    <a href="/index.php" style="color: white; "><h2 class="text-center">SISTEMA DE COBRANÇAS</h2></a>
 </div>
  
 
